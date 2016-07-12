f3012521a3f4ffba40c8c1116176e9a896c3a456
Abbie Holder <abigail.c.holder@gmail.com>
2016-07-11 20:29:22 -0500

    refactored cheer prefix

--- Program.cs
+++ Program.cs
@@ -9,12 +9,16 @@ namespace Cheers
     {
         public static void Main(string[] args)
         {
+            // Var section
+            String prompt = "> ";
+            String cheer_prefix = "Give me a";
+            
             Console.WriteLine("What's your name?");
-            Console.Write("> ");
+            Console.Write(prompt);
             String user_name = Console.ReadLine();
 
             for(int i = 0; i < user_name.Length; i++) {
-                Console.WriteLine("Give me a.. " + user_name[i]);
+                Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
             }
 
             Console.WriteLine(user_name.ToUpper() + " is.. GRAND!");
