7fcb68676e5040378bdad20509ef3f090c789731
Abbie Holder <abigail.c.holder@gmail.com>
2016-07-11 20:57:04 -0500

    if statement to decide whether char has vowel sound

--- Program.cs
+++ Program.cs
@@ -12,16 +12,22 @@ namespace Cheers
             // Var section
             String prompt = "> ";
             String cheer_prefix = "Give me a";
+            String requires_an = "halfnorsemix";
             
             Console.WriteLine("What's your name?");
             Console.Write(prompt);
-            String user_name = Console.ReadLine();
+            String user_name = Console.ReadLine().ToLower();
 
             for(int i = 0; i < user_name.Length; i++) {
-                Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
-            }
+                if (requires_an.IndexOf(user_name[i]) > -1) {
+                    // then it's in the requires_an array
+                    Console.WriteLine(cheer_prefix + "n.. " + user_name[i]);
+                } else {
+                    // doesn't require an
+                    Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
 
-            Console.WriteLine(user_name.ToUpper() + " is.. GRAND!");
+                }
+            }
         }
     }
 }
